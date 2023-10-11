aaf0509bd5b91728c0e4c4adfd24dbfd41fe4e72
Nabuchholz <nathaliabuchh@hotmail.com>
2023-10-11 11:41:18 -0300

    feat: add ptBR localization in root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Template Next App',
-  description: 'Template Nyx',
+  title: 'Tem Vaga Mestre',
+  description: 'Gerencie seus personagens e campanhas de rpg de maneira simples, rápida e eficiente',
 }
 
 export default function RootLayout({
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+
     <html lang='pt-Br'>
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider localization={ptBR}>
+        <body className={inter.className}>{children}</body>
+      </ClerkProvider>
     </html>
   )
 }
