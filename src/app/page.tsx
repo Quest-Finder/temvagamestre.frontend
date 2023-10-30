149df5dc0cc3d45ac6be5ad4e80d76715ba3c716
matheus mendes <matheusmendesreis@outlook.com>
2023-10-30 16:34:33 -0300

    #.env adicionado no gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import SingIn from "@/features/authentication/singIn";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-    </main>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
   )
 }
