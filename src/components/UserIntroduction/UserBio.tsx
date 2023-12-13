2617d99cd13658e4a4b8deec73ce7fb7d0a50724
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-12-13 19:26:24 -0300

    feat: add showMore funcion to bio paragraph

--- src/components/UserIntroduction/UserBio.tsx
+++ src/components/UserIntroduction/UserBio.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { useState } from 'react'
+import { Button } from '../ui/button'
+
 interface UserBioProps {
   children: string
 }
 
 export function UserBio({ children }: UserBioProps) {
-  return <p className='text-xl leading-6 text-zinc-500'>{children}</p>
+  const [showMore, setShowMore] = useState(false)
+
+  return (
+    <>
+      <p className='text-xl leading-6 text-zinc-500'>
+        {showMore ? children : `${children.substring(0, 321)}...`}
+      </p>
+      <Button
+        onClick={() => setShowMore(!showMore)}
+        variant='ghost'
+        className='my-3.5 ml-auto h-auto max-w-max p-0 text-base font-medium leading-none hover:bg-transparent'
+      >
+        Ver mais
+      </Button>
+    </>
+  )
 }
