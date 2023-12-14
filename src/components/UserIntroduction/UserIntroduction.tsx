1f0b6bacd79218edf31f10ec17fe887a6191c156
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-12-14 17:55:47 -0300

    feat: changed component to receive dynamic data

--- src/components/UserIntroduction/UserIntroduction.tsx
+++ src/components/UserIntroduction/UserIntroduction.tsx
@@ -1,24 +1,22 @@
 import { Separator } from '../ui/separator'
 import { UserInformation } from './UserInformation'
+import { UserIntroductionMock } from './mock'
 
 export function UserIntroduction() {
   return (
     <>
       <UserInformation.UserTitle
-        userName=''
-        userClass=''
-        userRole=''
+        userName={UserIntroductionMock.user.first_name}
+        userNickname={UserIntroductionMock.user.nickname}
+        userRole={UserIntroductionMock.user.role}
       />
       <Separator />
-      <UserInformation.Quote>Alguma frase muito leggal</UserInformation.Quote>
+      <UserInformation.Quote>
+        {UserIntroductionMock.user.info.quote}
+      </UserInformation.Quote>
       <UserInformation.Section>
         <UserInformation.SectionTitle title='Bio' />
-        <UserInformation.Bio>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          earum perferendis asperiores itaque culpa ratione nihil optio,
-          temporibus omnis doloremque dolor consequuntur, accusamus suscipit
-          labore quisquam, eum laudantium dolorum explicate.
-        </UserInformation.Bio>
+        <UserInformation.Bio description={UserIntroductionMock.user.info.bio} />
       </UserInformation.Section>
     </>
   )
