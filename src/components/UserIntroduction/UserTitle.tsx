fd05e7cb2495f138d8abb9d75dab43e0983f57aa
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-12-14 17:56:20 -0300

    refactor: changed prop name on User title

--- src/components/UserIntroduction/UserTitle.tsx
+++ src/components/UserIntroduction/UserTitle.tsx
@@ -1,15 +1,19 @@
 interface UserTitleProps {
   userName: string
   userRole: string
-  userClass: string
+  userNickname: string
 }
 
-export function UserTitle({ userName, userClass, userRole }: UserTitleProps) {
+export function UserTitle({
+  userName,
+  userNickname,
+  userRole,
+}: UserTitleProps) {
   return (
     <h1 className='mb-4 text-center text-2xl'>
       <span className='font-bold'>{`${userRole} ${userName} |`}</span>
       {` `}
-      {`${userClass}`}
+      {`${userNickname}`}
     </h1>
   )
 }
