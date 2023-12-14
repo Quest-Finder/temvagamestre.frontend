0fcfa5ffa68ea16694c7f210230c3c0a60d870d8
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-12-14 17:55:21 -0300

    feat: add mock file to resemble data coming

---
+++ src/components/UserIntroduction/mock.ts
@@ -0,0 +1,11 @@
+export const UserIntroductionMock = {
+  user: {
+    first_name: 'Rodrigo',
+    nickname: 'Elfo experiente',
+    role: 'Mestre',
+    info: {
+      bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantiumearum perferendis asperiores itaque culpa ratione nihil optio,temporibus omnis doloremque dolor consequuntur, accusamus suscipitlabore quisquam, eum laudantium dolorum explicate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantiumearum perferendis asperiores itaque culpa ratione nihil optio,temporibus omnis doloremque dolor consequuntur, accusamus suscipitlabore quisquam, eum laudantium dolorum explicate.',
+      quote: 'A vida não tem graça sem alguns dragões :)',
+    },
+  },
+}
