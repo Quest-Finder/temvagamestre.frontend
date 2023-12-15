c551e3f5e473e27dffdceb28406906dab552f536
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-12-15 19:13:36 -0300

    fix: resolve storybook import alias problem

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
@@ -18,6 +19,15 @@ const config: StorybookConfig = {
   },
   "docs": {
     "autodocs": "tag"
-  }
+  },
+  "webpackFinal": async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+      };
+    }
+    return config;
+  },
 };
 export default config;
