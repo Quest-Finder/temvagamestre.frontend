baee605188141e30ca05ce8ed439536dc5877e17
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-12-15 19:15:25 -0300

    feat: add mock file

---
+++ src/components/PlayerProfileSection/mock.ts
@@ -0,0 +1,19 @@
+import { GraphLegendItem } from '@/components/PlayerProfileSection/GraphLegend'
+
+export const MockGraphLegend: GraphLegendItem[] = [
+  {
+    description: 'Matar, Pilhar e Destruir',
+    value: 4,
+    id: 1,
+  },
+  {
+    description: 'Interpretação',
+    value: 2,
+    id: 2,
+  },
+  {
+    description: 'Dungeon',
+    value: 5,
+    id: 3,
+  },
+]
