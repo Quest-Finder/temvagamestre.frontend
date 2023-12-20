c90ad63a77ffc4130c6c024db03bba5be6a79122
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-12-19 21:05:20 -0300

    docs: changed design url to handoff figma page

--- src/components/UserIntroduction/intro.stories.tsx
+++ src/components/UserIntroduction/intro.stories.tsx
@@ -14,7 +14,7 @@ const meta: Meta<typeof UserInformation> = {
   parameters: {
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/file/0NbHKoUNsH7gssqSRbAq1n/Tem-vaga-mestre%3F?type=design&node-id=1306-4387&mode=design&t=1c3IVi2qXvIsaMTm-0',
+      url: 'https://www.figma.com/file/0NbHKoUNsH7gssqSRbAq1n/Tem-vaga-mestre%3F?type=design&node-id=1776-4508&mode=design&t=iN2EgkutdijNumqg-0',
     },
   },
 }
