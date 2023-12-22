62487cab2475a7765e04cee23081ca96bde5237d
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-12-22 18:25:49 -0300

    fix: fixed nickname condicional

--- src/features/page-profile/introduction/components/UserTitle.tsx
+++ src/features/page-profile/introduction/components/UserTitle.tsx
@@ -15,7 +15,7 @@ export function UserTitle({
         {`${userRole} ${userName}`}
         {userNickname && ` | `}
       </span>
-      {`${userNickname}`}
+      {userNickname && `${userNickname}`}
     </h1>
   )
 }
