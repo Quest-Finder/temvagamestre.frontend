c4b2e51d581718b17f66aeee99e50c299fa4df13
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-01-08 20:20:34 -0300

    refactor: changed function to filter graph data

--- src/features/page-profile/playerProfileSection/PlayerProfileSection.tsx
+++ src/features/page-profile/playerProfileSection/PlayerProfileSection.tsx
@@ -1,20 +1,15 @@
 import { Card } from '@/components/ui/card'
-import { filterOutItemWithoutData } from './helpers/filterOutItemWithoutData'
-import { reorderListInDescendingOrder } from './helpers/reorderListInDescendingOrder'
 import { MockGraphLegend } from './mock'
 import { PlayerProfile } from '.'
+import { filteredGraphData } from './helpers/filterGraphData'
 
 export function PlayerProfileSection() {
-  const filteredGraphData = reorderListInDescendingOrder(
-    filterOutItemWithoutData(MockGraphLegend),
-  )
-
   return (
     <PlayerProfile.Root>
       <PlayerProfile.Title title='Perfil do jogador' />
       <Card className='flex max-w-max items-center gap-4 border-neutral-100 px-4 py-6'>
-        <PlayerProfile.Graph graphData={filteredGraphData} />
-        <PlayerProfile.Legend graphData={filteredGraphData} />
+        <PlayerProfile.Graph graphData={filteredGraphData(MockGraphLegend)} />
+        <PlayerProfile.Legend graphData={filteredGraphData(MockGraphLegend)} />
       </Card>
     </PlayerProfile.Root>
   )
