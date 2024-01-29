7464192f43d35c3250502e50c4326c9c07b738b3
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-01-29 17:47:20 -0300

    feat: added conditional render on bio and quote

--- src/features/page-profile/introduction/introduction.stories.tsx
+++ src/features/page-profile/introduction/introduction.stories.tsx
@@ -32,36 +32,94 @@ export const FullIntroduction: Story = {
           userNickname={UserIntroductionMock.user.nickname}
           userRole={UserIntroductionMock.user.role}
         />
-        <Separator className='hidden sm:block' />
-        <UserIntroduction.Quote text={UserIntroductionMock.user.info.quote} />
-        <UserIntroduction.Section>
-          <UserIntroduction.SectionTitle
-            title='Bio'
-            className='hidden sm:block'
-          />
-          <UserIntroduction.Bio
-            description={UserIntroductionMock.user.info.bio}
-          />
-        </UserIntroduction.Section>
+        {UserIntroductionMock.user.info.quote && (
+          <>
+            <Separator className='mt-2 hidden sm:mt-4 sm:block' />
+            <UserIntroduction.Quote
+              text={UserIntroductionMock.user.info.quote}
+            />
+          </>
+        )}
+        {UserIntroductionMock.user.info.bio && (
+          <UserIntroduction.Section>
+            <UserIntroduction.SectionTitle
+              title='Bio'
+              className='hidden sm:block'
+            />
+            <UserIntroduction.Bio
+              description={UserIntroductionMock.user.info.bio}
+            />
+          </UserIntroduction.Section>
+        )}
       </Card>
     </>
   ),
 }
 
-export const MissingIntroduction: Story = {
+export const MissingQuote: Story = {
   render: () => (
-    <Card className='flex max-w-[862px] flex-col rounded-xl border-[1px] px-4 py-6 sm:px-6 sm:pt-4'>
-      <UserIntroduction.UserTitle
-        userName={UserIntroductionMock.user.first_name}
-        userRole='Jogador'
+    <>
+      <UserIntroduction.SectionTitle
+        title='Bio'
+        className='mb-4 block sm:hidden'
       />
-      <Separator className='hidden sm:block' />
-      <UserIntroduction.Section>
-        <UserIntroduction.SectionTitle title='Bio' />
-        <UserIntroduction.Bio
-          description={UserIntroductionMock.user.info.bio}
+      <Card className='flex max-w-[862px] flex-col rounded-xl border-[1px] px-4 py-6 sm:px-6 sm:pt-4'>
+        <UserIntroduction.UserTitle
+          userName={UserIntroductionMock.user.first_name}
+          userRole='Jogador'
         />
-      </UserIntroduction.Section>
-    </Card>
+        {'' && (
+          <>
+            <Separator className='mt-2 hidden sm:mt-4 sm:block' />
+            <UserIntroduction.Quote text='' />
+          </>
+        )}
+        {UserIntroductionMock.user.info.bio && (
+          <UserIntroduction.Section>
+            <UserIntroduction.SectionTitle
+              title='Bio'
+              className='hidden sm:block'
+            />
+            <UserIntroduction.Bio
+              description={UserIntroductionMock.user.info.bio}
+            />
+          </UserIntroduction.Section>
+        )}
+      </Card>
+    </>
+  ),
+}
+
+export const MissingBio: Story = {
+  render: () => (
+    <>
+      <UserIntroduction.SectionTitle
+        title='Bio'
+        className='mb-4 block sm:hidden'
+      />
+      <Card className='flex max-w-[862px] flex-col rounded-xl border-[1px] px-4 py-6 sm:px-6 sm:pt-4'>
+        <UserIntroduction.UserTitle
+          userName={UserIntroductionMock.user.first_name}
+          userRole='Jogador'
+        />
+        {UserIntroductionMock.user.info.quote && (
+          <>
+            <Separator className='mt-2 hidden sm:mt-4 sm:block' />
+            <UserIntroduction.Quote
+              text={UserIntroductionMock.user.info.quote}
+            />
+          </>
+        )}
+        {'' && (
+          <UserIntroduction.Section>
+            <UserIntroduction.SectionTitle
+              title='Bio'
+              className='hidden sm:block'
+            />
+            <UserIntroduction.Bio description='' />
+          </UserIntroduction.Section>
+        )}
+      </Card>
+    </>
   ),
 }
