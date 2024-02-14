ff03453b98c3f73a5a80a30a69cf99a805b4a8c6
Matheus <matheusmendesreis@outlook.com>
2024-02-14 19:05:47 -0300

    feat:corrigindo lint do arquivo useSendTokenToBackend.ts

--- src/features/page-profile/calendar/js/sendTokenToBackend.ts
+++ src/features/page-profile/calendar/js/sendTokenToBackend.ts
@@ -3,13 +3,10 @@ export function useSendTokenToBackend() {
     try {
       const response = await fetch('', {
         method: 'POST',
-        headers: {
-          'Content-Security-Policy-Report-Only':
-            'script-src https://accounts.google.com/gsi/client; frame-src https://accounts.google.com/gsi/; connect-src https://accounts.google.com/gsi/;',
-        },
+        headers: {},
         body: JSON.stringify({ token }),
       })
-      const data = await response.json()
+      await response.json()
     } catch (error) {}
   }
   return { sendTokenToBackend }
