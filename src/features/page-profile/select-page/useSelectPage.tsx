bf2b678d973bb9a8c992ca20ad9c9c9628d276ea
Matheus <matheusmendesreis@outlook.com>
2024-02-14 19:02:18 -0300

    feat:corringo eslint do arquivo useSelectPage.tsx

--- src/features/page-profile/select-page/useSelectPage.tsx
+++ src/features/page-profile/select-page/useSelectPage.tsx
@@ -1,32 +1,34 @@
-'use client'
-
 import { useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 const linksWithPath = [
   { text: 'perfil', path: `/user/profile?show=profile` },
   { text: 'imagens', path: `/user/profile?show=images` },
   { text: 'datas disponiveis', path: `/user/profile?show=dates` },
 ]
+
 export function useSelectPage() {
   type LinkTexts = (typeof linksWithPath)[number]['text']
 
   const [params, setParams] = useState<LinkTexts>('perfil')
   const searchParams = useSearchParams()
 
-  const handleSetParams = (state: LinkTexts) => {
-    setParams(() => state)
-  }
+  const handleSetParams = useCallback((state: LinkTexts) => {
+    setParams(state)
+  }, [])
 
-  const showElement = searchParams.get('show')
   useEffect(() => {
-    if (showElement === 'images') {
-      return handleSetParams('imagens')
+    const showElement = searchParams.get('show')
+    const updateParams = (element: string | null) => {
+      if (element === 'images') {
+        handleSetParams('imagens')
+      } else if (element === 'dates') {
+        handleSetParams('datas disponiveis')
+      }
     }
-    if (showElement === 'dates') {
-      return handleSetParams('datas disponiveis')
-    }
-  }, [params])
+
+    updateParams(showElement)
+  }, [searchParams, handleSetParams])
 
   return { handleSetParams, params, linksWithPath }
 }
