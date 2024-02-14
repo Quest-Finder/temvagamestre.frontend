45e7c65524c389e5014c9f6fcd10f3edf517e5c5
Matheus <matheusmendesreis@outlook.com>
2024-02-14 19:03:58 -0300

    feat:corrigindo eslint do arquivo GraphLegend.stories.tsx

--- src/features/page-profile/playerProfileSection/stories/GraphLegend.stories.tsx
+++ src/features/page-profile/playerProfileSection/stories/GraphLegend.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { MockGraphLegend } from '@/components/PlayerProfileSection/mock'
-import { PlayerProfileGraphLegend } from '../PlayerProfileGraphLegend'
+import { PlayerProfileGraphLegend } from '../components/PlayerProfileGraphLegend'
+import { MockGraphLegend } from '../mock'
 
 const meta: Meta<typeof PlayerProfileGraphLegend> = {
   title: 'PlayerProfileSection/PlayerProfileGraphLegend ',
