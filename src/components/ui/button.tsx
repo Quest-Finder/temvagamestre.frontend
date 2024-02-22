28fa4ef7ce638d6c1bfc3cd14515c828be638087
Matheus <matheusmendesreis@outlook.com>
2024-02-22 18:32:34 -0300

    refactor:delete unused style classes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,19 +10,12 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-primary-900  hover:bg-primary-900/95 text-neutral-50',
-        destructive:
-          'bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90',
-        outline:
-          'border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+          'bg-primary-900  hover:bg-primary-900/95 text-neutral-50 font-mono text-sm gap-2',
         secondary:
           'bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
-        ghost:
-          'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
-        link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
       },
       size: {
-        default: 'h-10 px-4 py-2',
+        default: 'h-10 px-8 py-4',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
