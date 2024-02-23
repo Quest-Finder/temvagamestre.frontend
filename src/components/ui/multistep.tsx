8c904068f63656506f42139a84f1afdf48e6fa47
Matheus <matheusmendesreis@outlook.com>
2024-02-22 22:31:28 -0300

    feat:create news components, multistep and check

--- src/components/ui/multistep.tsx
+++ src/components/ui/multistep.tsx
@@ -2,54 +2,68 @@ import React from 'react'
 import { CheckComponent } from '../icons/checkComponent'
 
 interface MultiStepProps {
-    size: number
-    currentStep?: number
-    arrayText?: string[]
+  size: number
+  currentStep?: number
+  arrayText?: string[]
 }
 
 function Multistep({ size, currentStep = 1, arrayText }: MultiStepProps) {
-    return (
-        <div className='flex items-center justify-center gap-2'>
-            {Array.from({ length: size }, (_, i) => i + 1).map(step => {
-                return (
-                    <div className='flex  items-center justify-center gap-2'>
-                        <div>
-                            <div
-                                key={step}
-                                className={`
-                            ${currentStep === step
-                                        ? 'border-primary-900 bg-primary-900'
-                                        : 'border-primary-600'
-                                    } 
+  return (
+    <div className='flex items-center justify-center gap-2'>
+      {Array.from({ length: size }, (_, i) => i + 1).map(step => {
+        return (
+          <div className='flex  items-center justify-center gap-2'>
+            <div>
+              <div
+                key={step}
+                className={`
+                            ${
+                              currentStep === step
+                                ? 'border-primary-900 bg-primary-900'
+                                : 'border-primary-600'
+                            } 
                             ${currentStep > step && 'bg-primary-600'} 
                             flex h-10 w-10 items-center justify-center rounded-full border`}
-                            >
-                                {currentStep <= step && (
-                                    <p
-                                        className={`${currentStep === step
-                                            ? 'text-neutral-50'
-                                            : 'text-primary-600'
-                                            }  m-4 text-center font-mono font-extrabold`}
-                                    >
-                                        {step}
-                                    </p>
-                                )}
-                                {currentStep >= step && <CheckComponent />}
-                            </div>
-                        </div>
-                        {arrayText && arrayText[step - 1] && (
-                            <p className='text-center font-mono text-primary-900 '>
-                                {arrayText[step - 1]}
-                            </p>
-                        )}
-                        {step !== size && (
-                            <div className='w-8  border border-neutral-200' />
-                        )}
-                    </div>
-                )
-            })}
-        </div>
-    )
+              >
+                {currentStep <= step && (
+                  <p
+                    className={`${
+                      currentStep === step
+                        ? 'text-neutral-50'
+                        : 'text-primary-600'
+                    }  m-4 text-center font-mono font-extrabold`}
+                  >
+                    {step}
+                  </p>
+                )}
+                {currentStep >= step && <CheckComponent />}
+              </div>
+            </div>
+            {arrayText && arrayText[step - 1] && (
+              <p className='text-center font-mono text-primary-900 '>
+                {arrayText[step - 1]}
+              </p>
+            )}
+            {step !== size && (
+              <div className='w-8  border border-neutral-200' />
+            )}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export { Multistep }
+// Example
+// ;<Multistep
+//   size={6}
+//   currentStep={3}
+//   arrayText={[
+//     'Dados do jogador',
+//     'Links',
+//     'Local',
+//     'Perfil do jogador',
+//     'Estilo de jogo',
+//   ]}
+// />
