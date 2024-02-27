b66e97a4bafb53ba8602629f10c3eb0c103e6847
Matheus <matheusmendesreis@outlook.com>
2024-02-27 12:34:09 -0300

    feat: component input estilizado

--- src/features/SocialNetworkRegistration/socialNetworkRegistration.tsx
+++ src/features/SocialNetworkRegistration/socialNetworkRegistration.tsx
@@ -1,39 +1,150 @@
-import React from 'react';
-import { TikTokIcon } from './icons/tiktok';
-import { TrashIcon } from './icons/trash';
+import React, { ElementType } from 'react'
+import { TikTokIcon } from './icons/tiktok'
+import { TrashIcon } from './icons/trash'
+import { VariantProps, cva } from 'class-variance-authority'
+import { cn } from '@/lib/utils'
 
-export default function SocialNetworkRegistration() {
-    return (
-        <>
-            <div className='flex gap-4 flex-col w-full'>
-                <div className='flex gap-4  w-full'>
-                    <div className='bg-[#222222] w-10 h-10 flex items-center justify-center rounded-md my-auto'>
-                        <TikTokIcon />
-                    </div>
-                    <div className='flex gap-2 flex-col  w-full'>
-                        <p>Url</p>
-                        <div className='flex items-center justify-center border border-neutral-300 py-2 px-3 rounded-md '>
-                            <p className='text-neutral-300'>https://www.tiktok.com/</p>
-                            <input type="text" placeholder='@firstname' className='text-neutral-800 placeholder-text-neutral-300 outline-none w-full' />
-                        </div>
-                    </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                    <div className='flex items-center gap-2'>
-                        <label htmlFor="toggle" className='inline-flex items-center cursor-pointer relative outline-none'>
-                            <input type="checkbox" id="toggle" className='peer sr-only outline-none' />
-                            <div className='peer-checked:bg-[#0F172A] bg-[#E2E8F0] cursor-pointer transition-all w-11 h-6  rounded-xl peer-focus:outline-none peer-focus:ring-[#0F172A]' />
-                            <span className='absolute w-5 h-5  top-[2px] left-1 transition-all rounded-full z-10 peer-checked:left-5 bg-[#FFFFFF] shadow-md' />
-                        </label>
-                        <p className='text-neutral-300'>Visibilidade</p>
-                    </div>
-                    <div className='flex items-center gap-2'>
-                        <TrashIcon />
-                        <p className='text-neutral-300'>Visibilidade</p>
-                    </div>
+interface SocialNetworkRootProps {
+  children: React.ReactNode
+  asCol?: boolean
+}
+function SocialNetworkBox({ children, asCol = false }: SocialNetworkRootProps) {
+  return (
+    <div className={`flex w-full gap-4 ${asCol && 'flex-col'}`}>{children}</div>
+  )
+}
 
-                </div>
-            </div>
-        </>
-    );
+const textVariants = cva('font-normal text-sm antialiased', {
+  variants: {
+    variant: {
+      default: 'text-neutral-300 ',
+      secondary: 'text-[#000000] uppercase',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+})
+interface SocialNetworkTextProps extends VariantProps<typeof textVariants> {
+  text: string
+  element?: ElementType
+  className?: string
+}
+
+function SocialNetworkText({
+  text,
+  element = 'p',
+  className,
+  variant,
+}: SocialNetworkTextProps) {
+  const Text = element
+  return (
+    <Text className={cn(textVariants({ variant, className }))}>{text}</Text>
+  )
+}
+
+interface SocialNetworkIconProps extends React.SVGProps<SVGSVGElement> {
+  icon: ElementType
+  text?: string
+}
+function SocialNetworkIcon({ icon, text, ...rest }: SocialNetworkIconProps) {
+  const Icon = icon
+  return (
+    <div className='flex items-center gap-2 '>
+      <Icon {...rest} />
+      {text && <SocialNetworkText text={text} />}
+    </div>
+  )
+}
+interface SocialNetworkInput
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  text?: string
+}
+function SocialNetworkInput({ text, ...restInput }: SocialNetworkInput) {
+  return (
+    <section className='flex items-center justify-center rounded-md border border-neutral-300 px-3 py-2 '>
+      {text && <SocialNetworkText text={text} />}
+      <input
+        {...restInput}
+        type='text'
+        className='placeholder-text-neutral-300 w-full text-neutral-800 outline-none'
+      />
+    </section>
+  )
+}
+interface SocialNetworkToggleInputProps
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  text?: string
+}
+
+function SocialNetworkToggleInput({
+  text,
+  id,
+  ...rest
+}: SocialNetworkToggleInputProps) {
+  return (
+    <div className='flex items-center justify-center gap-2'>
+      <label
+        htmlFor={id}
+        className='relative inline-flex cursor-pointer items-center outline-none'
+      >
+        <input
+          type='checkbox'
+          id={id}
+          className='peer sr-only outline-none'
+          {...rest}
+        />
+        <div className='h-6 w-11 cursor-pointer rounded-xl bg-[#E2E8F0] transition-all  peer-checked:bg-[#0F172A] peer-focus:outline-none peer-focus:ring-[#0F172A]' />
+        <span className='absolute left-1 top-[2px]  z-10 h-5 w-5 rounded-full bg-[#FFFFFF] shadow-md transition-all peer-checked:left-5' />
+      </label>
+      {text && <SocialNetworkText text={text} />}
+    </div>
+  )
+}
+
+// Example
+function SocialNetworkRegistration() {
+  return (
+    <>
+      <SocialNetworkBox asCol={true}>
+        <SocialNetworkBox>
+          <SocialNetworkIcon icon={TikTokIcon} />
+          <SocialNetworkBox asCol={true}>
+            <SocialNetworkText
+              text='url'
+              variant='secondary'
+            />
+            <SocialNetworkInput
+              text='https://www.tiktok.com/'
+              placeholder='@firstname'
+            />
+          </SocialNetworkBox>
+        </SocialNetworkBox>
+        <div className='flex items-center justify-between gap-2'>
+          <SocialNetworkToggleInput
+            text='Visibilidade'
+            id='toggle'
+          />
+          <SocialNetworkIcon
+            icon={TrashIcon}
+            text='Visibilidade'
+          />
+        </div>
+      </SocialNetworkBox>
+    </>
+  )
+}
+
+export {
+  SocialNetworkRegistration,
+  SocialNetworkIcon,
+  SocialNetworkBox,
+  SocialNetworkInput,
+  SocialNetworkText,
 }
