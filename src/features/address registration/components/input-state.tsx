ec939c47eb81dd142da5f9908409d06c13195860
Matheus <matheusmendesreis@outlook.com>
2024-03-01 12:00:28 -0300

    feat: adicionando funçẽs que altera valor do formulario

---
+++ src/features/address registration/components/input-state.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useFormContext } from 'react-hook-form'
+import { FormField, FormLabel } from '@/components/ui/form'
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+import { addressRegistrationValidationT } from '../types/address-registration'
+import { states } from '../utils/estates'
+
+
+export default function InputState() {
+    const form = useFormContext<addressRegistrationValidationT>()
+
+    return (
+        <FormField
+            name='state'
+            control={form.control}
+            render={({ field }) => (
+                <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                >
+                    <FormLabel
+                        className={` ${form.formState.errors.state && 'text-red-500'} `}
+                    >
+                        Em qual estado você mora?
+                    </FormLabel>
+
+                    <SelectTrigger className='mt-2'>
+                        <SelectValue
+                            placeholder='Selecione seu estado'
+                            onClick={() => form.clearErrors('state')}
+                        />
+                    </SelectTrigger>
+                    <SelectContent className=' bg-neutral-50'>
+                        <SelectGroup>
+                            {states.map(option => (
+                                <SelectItem
+                                    key={option.value}
+                                    value={option.value}
+                                >
+                                    {option.label}
+                                </SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            )}
+        />
+    )
+}
