a392f0d96f3dca98622078e0b06ec6100781945d
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-03-07 21:48:24 -0300

    fix: corrected month array for select

--- src/features/register/components/DateOfBirth.tsx
+++ src/features/register/components/DateOfBirth.tsx
@@ -9,25 +9,27 @@ import {
 import { useEffect, useState } from 'react'
 
 const Meses = [
-  { name: 'Janeiro', value: 1 },
-  { name: 'Fevereiro', value: 2 },
-  { name: 'Março', value: 3 },
-  { name: 'Abril', value: 4 },
-  { name: 'Maio', value: 5 },
-  { name: 'Junho', value: 6 },
-  { name: 'Julho', value: 7 },
-  { name: 'Agosto', value: 8 },
-  { name: 'Setembro', value: 9 },
-  { name: 'Outubro', value: 10 },
-  { name: 'Novembro', value: 11 },
-  { name: 'Dezembro', value: 12 },
+  { name: 'Janeiro', value: '01' },
+  { name: 'Fevereiro', value: '02' },
+  { name: 'Março', value: '03' },
+  { name: 'Abril', value: '04' },
+  { name: 'Maio', value: '05' },
+  { name: 'Junho', value: '06' },
+  { name: 'Julho', value: '07' },
+  { name: 'Agosto', value: '08' },
+  { name: 'Setembro', value: '09' },
+  { name: 'Outubro', value: '10' },
+  { name: 'Novembro', value: '11' },
+  { name: 'Dezembro', value: '12' },
 ]
 
 function generateDayFromMonth() {
   const days = []
   // eslint-disable-next-line no-plusplus
   for (let i = 1; i <= 31; i++) {
-    days.push(i)
+    if (i < 10) {
+      days.push(`0${i}`)
+    } else days.push(i)
   }
   return days
 }
