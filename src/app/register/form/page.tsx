67b0057eaecaede02552ad55a5223ba23cec7a00
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-03-09 10:02:58 -0300

    feat: add form field page to visualize the form

---
+++ src/app/register/form/page.tsx
@@ -0,0 +1,10 @@
+import { FormSetpOne } from '@/features/register/FormStepOne'
+import { FormLayout } from '@/features/register/FormLayout'
+
+export default function Form() {
+  return (
+    <FormLayout>
+      <FormSetpOne />
+    </FormLayout>
+  )
+}
