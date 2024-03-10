3a663e8ed08cb4b1bca19aa1084e28ef396b924e
Matheus <matheusmendesreis@outlook.com>
2024-03-10 13:17:47 -0300

    fix:correção dos icones que devem ser exibidos

--- src/features/page-profile/user-description/components/boxWithIcons.tsx
+++ src/features/page-profile/user-description/components/boxWithIcons.tsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import facebook from '@/assets/social-medias/facebook.svg'
-import instagram from '@/assets/social-medias/instagram.svg'
-import reedit from '@/assets/social-medias/reedit.svg'
-import tiktok from '@/assets/social-medias/tiktok.svg'
-import whatsapp from '@/assets/social-medias/whatsapp.svg'
-import Image from 'next/image'
+import FacebookIcon from '@/components/icons/facebookIcon'
+import { InstagramIcon } from '@/components/icons/instagram'
+import ReeditIcon from '@/components/icons/reeditIcon'
+import XIcon from '@/components/icons/xIcon'
+import React, { ElementType } from 'react'
 
-const socialMedia = [tiktok, instagram, facebook, whatsapp, reedit]
+const socialMedia: ElementType[] = [
+  InstagramIcon,
+  XIcon,
+  FacebookIcon,
+  ReeditIcon,
+]
 export function BoxWithIcons() {
   return (
     <section className='flex items-center justify-center gap-3'>
-      {socialMedia.map(icon => (
-        <div className='flex h-11 w-11 items-center justify-center rounded-md bg-black p-3'>
-          <Image
-            src={icon}
-            alt=''
-            width={100}
-            height={100}
-            quality={100}
-          />
-        </div>
-      ))}
+      {socialMedia.map(icon => {
+        const IconName = icon
+        return (
+          <IconName className='my-auto flex h-10 w-10 items-center justify-center gap-2 rounded-md bg-[#222222] p-2' />
+        )
+      })}
     </section>
   )
 }
