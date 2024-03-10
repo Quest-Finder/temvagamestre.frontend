a5ed6ccd749e4dfd11725efa37121766e39a2197
Matheus <matheusmendesreis@outlook.com>
2024-03-10 12:32:38 -0300

    styled: ajustando responsividade

--- src/features/page-profile/box-image/components/cardWithImage.tsx
+++ src/features/page-profile/box-image/components/cardWithImage.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import { Suspense } from 'react'
 
 interface CardWithImgProps {
   urlImgs: string[]
@@ -8,21 +7,17 @@ export function CardWithImg({ urlImgs }: CardWithImgProps) {
   return (
     <section
       key={new Date().getTime().toString()}
-      className='flex items-center justify-center  gap-4  overflow-x-auto rounded-xl pr-1'
+      className='flex items-center justify-center  gap-4  overflow-x-auto rounded-xl pl-[68%] pr-1'
     >
       {urlImgs.map(url => (
-        <Suspense
-          fallback={<div className='h-52 w-64 bg-red-600'>Loading...</div>}
-        >
-          <Image
-            src={url}
-            alt=''
-            width={259}
-            height={208}
-            className='rounded-xl'
-            loading='lazy'
-          />
-        </Suspense>
+        <Image
+          src={url}
+          alt=''
+          width={259}
+          height={208}
+          className='rounded-xl'
+          loading='lazy'
+        />
       ))}
     </section>
   )
