15f659bc463cfc1746445d5a4fb4cca023c1b1c7
Matheus <matheusmendesreis@outlook.com>
2024-04-04 15:38:03 -0300

    feat:create types addressRegistrationValidationT

---
+++ src/features/address registration/types/address-registration.d.ts
@@ -0,0 +1,3 @@
+import { addressRegistrationValidation } from "../utils/address-registration-validation";
+
+export type addressRegistrationValidationT = z.infer<typeof addressRegistrationValidation>
