b2001c1292d6acbac97d37cbde37b9a809fc2560
Matheus <matheusmendesreis@outlook.com>
2024-04-05 19:26:06 -0300

    refactor/outra forma de criar o hook form

--- src/features/social network registration/hook/useFormSocialNetWork.tsx
+++ src/features/social network registration/hook/useFormSocialNetWork.tsx
@@ -7,13 +7,14 @@ import { socialNetworkValidation } from '../utils/social-network-registration-va
 import { DEFAULT_VALUES_SOCIAL_MEDIA } from '../utils/defaul-values-social-media'
 
 export default function useFormSocialMedia() {
+  // recupera os valores do local storage
   // const parsedData = useLocalStorageGetItem(
   //     'form_data_adress',
   //     DEFAULT_VALUES_SOCIAL_MEDIA,
   // )
-  const form = useForm<TsocialNetworkValidation>({
+
+  return useForm<TsocialNetworkValidation>({
     resolver: zodResolver(socialNetworkValidation),
     defaultValues: DEFAULT_VALUES_SOCIAL_MEDIA,
   })
-  return { form }
 }
--- src/features/social network registration/hook/useSubmitSocialNetWokr.tsx
+++ src/features/social network registration/hook/useSubmitSocialNetWokr.tsx
@@ -1,20 +1,13 @@
 import { useCallback } from 'react'
-import { UseFormReturn } from 'react-hook-form'
-import { useRouter } from 'next/navigation'
 import { TsocialNetworkValidation } from '../types/social-network'
 
-export default function useSubmitFormAddressRegistration(
-  form: UseFormReturn<TsocialNetworkValidation>,
-  navigateToPlayerProfileUrl: string,
-) {
-  const router = useRouter()
-
+export default function useSubmitFormAddressRegistration(url: string) {
   return useCallback(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     (values: TsocialNetworkValidation) => {
       // uselocalStorageSetItem('form_data_adress', values)
-      router.push(navigateToPlayerProfileUrl)
+      // router.push(url)
     },
-    [navigateToPlayerProfileUrl, router],
+    [],
   )
 }
