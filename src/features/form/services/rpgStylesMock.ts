2b8d1f25306a0e81353f24336f94af842ca85295
NaBuchholz <nathaliabuchh@hotmail.com>
2024-04-05 14:22:04 -0300

    feat: mock rpg styles

---
+++ src/features/form/services/rpgStylesMock.ts
@@ -0,0 +1,12 @@
+export const rpgStylesArray: string[] = [
+  'Fantasia',
+  'Fantasia Heróica',
+  'Ninja vs Samurai',
+  'Espada e Feitiçaria',
+  'Fantasia Épica',
+  'Fantasia Mística',
+  'Fantasia Sombria',
+  'Intriga',
+  'Guerra',
+  'Nave Mãe',
+]
