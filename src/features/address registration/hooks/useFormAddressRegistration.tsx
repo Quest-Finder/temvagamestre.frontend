ca847f4712f59c6e6441ae9070778adbebf942fd
Matheus <matheusmendesreis@outlook.com>
2024-04-07 17:21:10 -0300

    refactor:isoletad useEffects

--- src/features/address registration/hooks/useFormAddressRegistration.tsx
+++ src/features/address registration/hooks/useFormAddressRegistration.tsx
@@ -1,43 +1,23 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
 import { addressRegistrationValidationT } from '../types/address-registration'
-import { DEFAULT_VALEUS_ADDRESS_REGISTRATION } from '../utils/defaul-values'
 import { addressRegistrationValidation } from '../utils/address-registration-validation'
+import { DEFAULT_VALEUS_ADDRESS_REGISTRATION } from '../utils/defaul-values'
+import ResetCityFieldWhenStateChanges from './resetCityFieldWhenStateChanges'
 import useLocalStorageGetItem from './useLocalStorageGetItem'
-import useSubmitFormAddressRegistration from './useSubmitFormAddressRegistration'
+import { useResetFieldsWhenLiveInBrazil } from './useResetFieldsWhenLiveInBrazil'
+import { useSetLiveInBrazilWhenStateOrCitySet } from './useSetLiveInBrazilWhenStateOrCitySet'
 
 export default function useFormAddressRegistration() {
-  const parsedData = useLocalStorageGetItem(
-    'form_data_adress',
-    DEFAULT_VALEUS_ADDRESS_REGISTRATION,
-  )
+  const parsedData = useLocalStorageGetItem('form_data_adress')
   const form = useForm<addressRegistrationValidationT>({
     resolver: zodResolver(addressRegistrationValidation),
-    defaultValues: parsedData,
+    defaultValues: parsedData || DEFAULT_VALEUS_ADDRESS_REGISTRATION,
   })
+  useResetFieldsWhenLiveInBrazil(form)
+  useSetLiveInBrazilWhenStateOrCitySet(form)
+  ResetCityFieldWhenStateChanges(form)
 
-  const liveInBrazil = form.watch('liveInBrazil')
-  const state = form.watch('state')
-  const city = form.watch('city')
-
-  React.useEffect(() => {
-    if (form.watch('liveInBrazil') === false) {
-      return
-    }
-    form.setValue('state', '')
-    form.setValue('city', '')
-  }, [liveInBrazil, form])
-
-  React.useEffect(() => {
-    if (form.watch('state') !== '' || form.watch('city') !== '') {
-      form.setValue('liveInBrazil', false)
-    }
-  }, [state, city, form])
-  const onSubmit = useSubmitFormAddressRegistration(
-    form,
-    '/cadastro/perfil-de-jogador',
-  )
-  return { form, onSubmit }
+  return { form }
 }
