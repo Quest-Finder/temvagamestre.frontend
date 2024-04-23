1af385b71837169c620c7b32d6711cd9801bf59c
Matheus <matheusmendesreis@outlook.com>
2024-04-23 13:07:51 -0300

    feat: add fecth citys of brazil

---
+++ src/features/register-player/address registration/service/get-city-by-estate.tsx
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export interface ICidade {
+  id: string
+  nome: string
+}
+
+interface GetCityByEstateProps {
+  uf: string
+}
+export default function GetCityByEstate({ uf }: GetCityByEstateProps) {
+  const [data, setData] = useState<ICidade[]>([])
+  const [isLoading, setLoading] = useState(true)
+
+  const fetchData = useCallback(async () => {
+    const path = `${process.env.NEXT_PUBLIC_API_CIDADES_URL}localidades/estados/${uf}/distritos`
+    try {
+      const res = await fetch(path)
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`)
+      }
+
+      const resJson: ICidade[] = await res.json()
+      setData(resJson)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+    }
+  }, [uf])
+
+  useEffect(() => {
+    if (!uf) return
+    fetchData()
+  }, [uf, fetchData])
+
+  return { data, isLoading }
+}
