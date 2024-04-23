a849d6fb525ff1445d0108120ad89939bfb516d0
Matheus <matheusmendesreis@outlook.com>
2024-04-23 13:16:35 -0300

    feat: add hook form about you

---
+++ src/features/register-player/about-you/hooks/useHookFormAboutYou.tsx
@@ -0,0 +1,16 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import useLocalStorageGetItem from '../../address registration/hooks/useLocalStorageGetItem'
+import { TValidation } from '../types/validation'
+import { defaultValuesAboutYou } from '../utils/default-values'
+import { validateAboutYou } from '../utils/validation'
+
+export default function useHookFormAboutYou() {
+  const parsedData = useLocalStorageGetItem('form_data_about_you')
+
+  const form = useForm<TValidation>({
+    resolver: zodResolver(validateAboutYou),
+    defaultValues: parsedData || defaultValuesAboutYou,
+  })
+  return form
+}
