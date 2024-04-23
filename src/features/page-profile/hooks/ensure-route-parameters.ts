d1eb11922cf120f459876d66f9b9935f9a9b8117
Matheus <matheusmendesreis@outlook.com>
2024-04-23 13:18:32 -0300

    feat:ensure path in url

---
+++ src/features/page-profile/hooks/ensure-route-parameters.ts
@@ -0,0 +1,25 @@
+'use client'
+
+import useWindowDimensions from '@/hooks/useWindowWidth'
+import { PublicRoutes } from '@/services/routers'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function useEnsureRouteParams() {
+  const router = useRouter()
+  const windowSize = useWindowDimensions()
+
+  const searchParams = useSearchParams()
+  type ShowElement = 'profile' | 'images' | 'dates' | null
+  const showElement: ShowElement = searchParams.get('show') as ShowElement
+  useEffect(() => {
+    if (windowSize.width < 768 && !showElement) {
+      router.push(`${PublicRoutes.PlayerProfile}?show=profile`)
+    }
+    if (windowSize.width > 768) {
+      router.push(`${PublicRoutes.PlayerProfile}`)
+    }
+  }, [router, showElement, windowSize.width])
+
+  return showElement
+}
