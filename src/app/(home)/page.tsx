d0ed5cae55a1aede6e27d58d4718af5e52cec827
Matheus <matheusmendesreis@outlook.com>
2024-04-23 13:02:10 -0300

    refactor:adjust the routers

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,16 @@
-import FormAboutYou from '@/features/about-you/form-about-you'
+import { PublicRoutes, RegisterRoutes } from '@/services/routers'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='flex flex-col gap-3'>
-        <Link href='/sign-in'>
-          <Button>sign-in</Button>
-        </Link>
-        <Link href='/user/profile?show=profile'>
-          <Button>profile</Button>
-        </Link>
-        <Link href='/register/form?show=form'>
-          <Button>formulario</Button>
-        </Link>
+        <Link href={PublicRoutes.SignIn}>sign-in</Link>
+        <Link href={PublicRoutes.PlayerProfile}>perfil do jogador</Link>
+        <Link href={RegisterRoutes.PlayerData}>dados jogador</Link>
+        <Link href={RegisterRoutes.SocialNetworks}>redes sociais</Link>
+        <Link href={RegisterRoutes.AboutYou}>sobre voce</Link>
+        <Link href={RegisterRoutes.Adress}>endereço</Link>
       </div>
     </main>
   )
