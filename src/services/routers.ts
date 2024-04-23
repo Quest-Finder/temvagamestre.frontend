107ac4eb5caf347e012930c84331cf9660b65d9c
Matheus <matheusmendesreis@outlook.com>
2024-04-23 12:37:40 -0300

    feat:create fied with router enums

---
+++ src/services/routers.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-shadow */
+export enum PublicRoutes {
+  Home = '/',
+  PlayerProfile = '/user/profile',
+  SignIn = '/sign-in',
+}
+
+export enum RegisterRoutes {
+  PlayerData = '/cadastro/dados-jogador',
+  SocialNetworks = '/cadastro/redes-sociais',
+  AboutYou = '/cadastro/sobre-voce',
+  Adress = '/cadastro/local',
+  StylesPlay = '/cadastro/estilos-de-jogo',
+}
+
+export const publicRoutes = [
+  ...Object.values(PublicRoutes),
+  ...Object.values(RegisterRoutes),
+]
