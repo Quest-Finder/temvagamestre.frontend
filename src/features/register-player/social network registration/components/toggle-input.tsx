4bca08b3a2411cece0dfacaacd084258ed5334b3
Matheus <matheusmendesreis@outlook.com>
2024-04-23 12:56:03 -0300

    lint: lint style aply

--- src/features/register-player/social network registration/components/toggle-input.tsx
+++ src/features/register-player/social network registration/components/toggle-input.tsx
@@ -1,8 +1,8 @@
 import {
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Toogle } from '@/components/ui/toggle'
