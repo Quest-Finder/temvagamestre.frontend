d58937a72ffab88d987537bbdefbeaa950ef6d89
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-05-02 20:31:57 -0300

    feat: added mock array to have data to make the page

---
+++ src/features/register-player/game-styles/mock.ts
@@ -0,0 +1,51 @@
+interface GameStyle {
+  id: string,
+  name: string
+}
+
+export const mockGameStyles: GameStyle[] = [
+  {
+    "id": "310e13e3-7e81-4410-99a9-74363eeaa593",
+    "name": "Fantasia"
+  },
+  {
+    "id": "7f98001f-c358-43d0-93f0-6df1fb926a15",
+    "name": "Fantasia Heroica"
+  },
+  {
+    "id": "2a034063-4c21-47b7-b4b9-608ce37530a6",
+    "name": "Fantasia Épica"
+  },
+  {
+    "id": "ebc0b866-a783-4e39-b839-b4bc9572f61a",
+    "name": "Fantasia Mítica"
+  },
+  {
+    "id": "bbae56fc-2136-4a34-9e14-2ac38755e283",
+    "name": "Fantasia Sombria"
+  },
+  {
+    "id": "dbc67ae8-7176-4127-8b1f-343486d2049f",
+    "name": "Ninja vs Samurai"
+  },
+  {
+    "id": "a5b8b402-792c-4746-9116-7b3b2202f370",
+    "name": "Espada e Feitiçaria"
+  },
+  {
+    "id": "df1727d9-ec70-43b2-94f4-498a95dffd7d",
+    "name": "Intriga"
+  },
+  {
+    "id": "44d1da09-c3a3-41b9-9ecc-4b07834a12d8",
+    "name": "Guerra"
+  },
+  {
+    "id": "ef07e682-53ba-4661-8d82-9bee7c21c82b",
+    "name": "Nave Mãe"
+  },
+  {
+    "id": "26d4f74e-0469-489e-b35a-3896625ddfe9",
+    "name": "Mistério"
+  }
+]
