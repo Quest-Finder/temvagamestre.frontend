1fba4026cefab4f7055642ed129c83579eb65cff
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-05-03 21:51:01 -0300

    refactor: small optimization on validationLimitationCheck

--- src/features/register-player/game-styles/helpers/validationCheckBoxLimitation.ts
+++ src/features/register-player/game-styles/helpers/validationCheckBoxLimitation.ts
@@ -1,6 +1,3 @@
 export function validationCheckBoxLimitation(array: string[], field: string, maxNumber: number) {
-  if (array.length >= maxNumber && !array.includes(field)) {
-   return true
-  }
-  return false
+  return array.length >= maxNumber && !array.includes(field);
 }
