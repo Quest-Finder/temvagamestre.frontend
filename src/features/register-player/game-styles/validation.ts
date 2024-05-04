85276aae894655006adfa439d83e97e19ced4cf7
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-05-03 21:43:32 -0300

    feat: add checkbox limit up to 3

--- src/features/register-player/game-styles/validation.ts
+++ src/features/register-player/game-styles/validation.ts
@@ -1,8 +1,11 @@
 import * as z from 'zod'
 
 export const FormGameStylesSchema = z.object({
-  items: z.array(z.string()).refine((value) => value.some((item) => item), {
+  rpgStyles: z.array(z.string()).refine((value) => value.some((item) => item), {
     message: "You have to select at least one item.",
+  })
+    .refine((array) => array.length <=  3, {
+    message: "No máximo 3 opções"
   }),
 })
 
