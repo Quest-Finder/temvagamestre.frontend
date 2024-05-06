2774a4ccc207bed83b3c715659b01a19ecb42398
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-05-06 18:46:07 -0300

    feat: add error message to validation

--- src/features/register-player/player-profile/validation.ts
+++ src/features/register-player/player-profile/validation.ts
@@ -2,6 +2,7 @@ import { z } from "zod"
 
 export const FormPlayerProfileSchema = z.object({
   playerProfileId: z.string()
+    .min(1, { message: 'Necessário escolher pelo menos uma opção' })
 })
 
 export type FormPlayerProfileType = z.infer<typeof FormPlayerProfileSchema>
