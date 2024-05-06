da96b334730cedd29010562d3fea9cd2518165c4
Matheus <matheusmendesreis@outlook.com>
2024-05-06 20:58:02 -0300

    feat:update validation

--- src/features/register-player/about-you/utils/validation.ts
+++ src/features/register-player/about-you/utils/validation.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
 export const validateAboutYou = z.object({
-  titulo: z.string().min(1).max(50).default(''),
-  bio: z.string().min(1).max(500).default(''),
+  titulo: z.string().max(500).default(''),
+  bio: z.string().max(500).default(''),
 })
