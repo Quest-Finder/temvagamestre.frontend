1f75cec0a7f2833ad63ed21a74c7608e8168fb56
Matheus <matheusmendesreis@outlook.com>
2024-05-14 20:32:07 -0300

    refactor: correçoes do lint

--- src/features/register-player/player-registration/validation.ts
+++ src/features/register-player/player-registration/validation.ts
@@ -1,6 +1,6 @@
-import * as z from 'zod'
-import moment from 'moment'
 import { filter } from '@/helpers/badWordsFilter/badWordsFilter'
+import moment from 'moment'
+import * as z from 'zod'
 import { checkUniqueUsername } from './helper/checkUniqueUsername'
 
 export const FormStepOneSchema = z.object({
@@ -19,8 +19,8 @@ export const FormStepOneSchema = z.object({
     .min(1, 'Campo obrigatório')
     .max(15, 'O username deve conter no máximo 15 caracteres')
     .regex(/^[a-zA-Z0-9'-]*$/, 'Caracteres especiais não são disponiveis')
-    .refine((username) => checkUniqueUsername(username), {
-      message: "Username ja existente"
+    .refine(username => checkUniqueUsername(username), {
+      message: 'Username ja existente',
     })
     .refine(username => !filter.isProfane(username), {
       message: 'Palavras de baixo calão não são permitidas',
