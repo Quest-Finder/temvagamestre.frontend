54eefa18de5bba6e624566e9bf2b2efbb11fdc17
Matheus <matheusmendesreis@outlook.com>
2024-05-14 20:29:16 -0300

    refactor: reorder imports and fix styling in FormPlayerProfile

--- src/features/register-player/player-profile/FormPlayerProfile.tsx
+++ src/features/register-player/player-profile/FormPlayerProfile.tsx
@@ -1,24 +1,23 @@
 'use client'
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-} from "@/components/ui/form"
-import { Card, CardTitle, CardDescription } from '@/components/ui/card'
-import { Button } from "@/components/ui/button"
 import { Arrow } from '@/components/icons/Arrow'
-import { useForm } from "react-hook-form"
-import { FormPlayerProfileType, FormPlayerProfileSchema } from './validation'
-import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from '@/components/ui/button'
+import { Card, CardDescription, CardTitle } from '@/components/ui/card'
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import Image from 'next/image'
+import { useForm } from 'react-hook-form'
+import { FormPlayerProfileSchema, FormPlayerProfileType } from './validation'
 
 import { mockFormPlayerProfile } from './mock'
 
-import { getRpgImage, getRpgStyling, getRpgImageColor } from './helpers/getRpgStyle'
-import { FormTitle } from "./components/FormTitle"
-import { FormAditionalText } from "./components/FormAditionalText"
+import { FormAditionalText } from './components/FormAditionalText'
+import { FormTitle } from './components/FormTitle'
+import {
+  getRpgImage,
+  getRpgImageColor,
+  getRpgStyling,
+} from './helpers/getRpgStyle'
 
 export function FormPlayerProfile() {
   const form = useForm<FormPlayerProfileType>({
@@ -28,27 +27,27 @@ export function FormPlayerProfile() {
     },
   })
 
-  function onSubmit(data: FormPlayerProfileType) {
-    console.log(data)
-  }
-
-  console.log(form.getValues('playerProfileId'))
+  function onSubmit() {}
 
   return (
     <Form {...form}>
       <FormAditionalText>
-        Olá! Para aprimorarmos sua experiência no TVM, por favor, responda algumas perguntas.
+        Olá! Para aprimorarmos sua experiência no TVM, por favor, responda
+        algumas perguntas.
       </FormAditionalText>
       <FormTitle>
         Escolha o perfil de jogador que mais se alinha com seus interesses.
       </FormTitle>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex flex-col items-center">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex flex-col items-center space-y-8'
+      >
         <FormField
           control={form.control}
           name='playerProfileId'
           render={() => (
-            <FormItem className="flex gap-[1.625rem] space-y-0 max-w-[1125px]">
-              {mockFormPlayerProfile.map((item) => (
+            <FormItem className='flex max-w-[1125px] gap-[1.625rem] space-y-0'>
+              {mockFormPlayerProfile.map(item => (
                 <FormField
                   key={item.id}
                   control={form.control}
@@ -57,25 +56,35 @@ export function FormPlayerProfile() {
                     return (
                       <FormItem
                         key={item.id}
-                        className="basis-1/3"
+                        className='basis-1/3'
                       >
                         <FormControl>
                           <Card
-                            aria-disabled={form.getValues("playerProfileId").length !== 0 && (form.getValues('playerProfileId') !== item.id)}
-                            className={`${getRpgStyling(item.title)} py-4 px-6 aria-disabled:opacity-50 cursor-pointer`}
+                            aria-disabled={
+                              form.getValues('playerProfileId').length !== 0 &&
+                              form.getValues('playerProfileId') !== item.id
+                            }
+                            className={`${getRpgStyling(
+                              item.title,
+                            )} cursor-pointer px-6 py-4 aria-disabled:opacity-50`}
                             onClick={() => {
-                              console.log(form.getValues('playerProfileId'))
                               return field.onChange(item.id)
                             }}
                           >
-                            <div className={`${getRpgImageColor(item.title)} rounded-full p-4 h-24 w-24 flex justify-center items-center`}>
+                            <div
+                              className={`${getRpgImageColor(
+                                item.title,
+                              )} flex h-24 w-24 items-center justify-center rounded-full p-4`}
+                            >
                               <Image
                                 src={getRpgImage(item.title)}
                                 alt={item.title}
                               />
                             </div>
                             <CardTitle>{item.title}</CardTitle>
-                            <CardDescription>{item.description}</CardDescription>
+                            <CardDescription>
+                              {item.description}
+                            </CardDescription>
                           </Card>
                         </FormControl>
                       </FormItem>
