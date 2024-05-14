0f7b9390285ae97ecfaa03e263f43c2777ef7c15
Matheus <matheusmendesreis@outlook.com>
2024-05-14 20:31:31 -0300

    refactor: correçoes do lint

--- src/features/register-player/player-profile/helpers/getRpgStyle.ts
+++ src/features/register-player/player-profile/helpers/getRpgStyle.ts
@@ -1,15 +1,17 @@
 import Exploracao from '../assets/Exploracao.svg'
-import MatarEPilhar from '../assets/MatarEPilhar.svg'
 import Interpretacao from '../assets/Interpretacao.svg'
+import MatarEPilhar from '../assets/MatarEPilhar.svg'
 
 export function getRpgImage(rpgStyle: string) {
   switch (rpgStyle) {
     case 'Exploração':
       return Exploracao
-    case 'Matar e pilhar': 
+    case 'Matar e pilhar':
       return MatarEPilhar
     case 'Interpretação':
       return Interpretacao
+    default:
+      return ''
   }
 }
 
@@ -17,10 +19,12 @@ export function getRpgImageColor(rpgStyle: string) {
   switch (rpgStyle) {
     case 'Exploração':
       return 'bg-primary-100'
-    case 'Matar e pilhar': 
+    case 'Matar e pilhar':
       return 'bg-secondary-100'
     case 'Interpretação':
       return 'bg-accent-100'
+    default:
+      return ''
   }
 }
 
@@ -28,9 +32,11 @@ export function getRpgStyling(rpgStyle: string) {
   switch (rpgStyle) {
     case 'Exploração':
       return 'border-primary-500 bg-primary-50'
-    case 'Matar e pilhar': 
+    case 'Matar e pilhar':
       return 'border-secondary-500 bg-secondary-50'
     case 'Interpretação':
       return 'border-accent-500 bg-accent-50'
+    default:
+      return ''
   }
 }
