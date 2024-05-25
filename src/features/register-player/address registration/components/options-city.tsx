4d45f420bf4feed2f6fd513d4f519312de6af9c4
Matheus <matheusmendesreis@outlook.com>
2024-05-25 14:00:29 -0300

    feat: Update OptionsCity component to include disalbe in button

--- src/features/register-player/address registration/components/options-city.tsx
+++ src/features/register-player/address registration/components/options-city.tsx
@@ -1,3 +1,4 @@
+import Search from '@/components/icons/searchIcon'
 import { useFormContext } from 'react-hook-form'
 import GetCityByEstate from '../service/get-city-by-estate'
 import { addressRegistrationValidationT } from '../types/address-registration'
@@ -19,39 +20,39 @@ export default function OptionsCity({
   cityValue,
 }: OptionsCityProps) {
   const { data, isLoading } = GetCityByEstate({ uf })
+  const disable = true
   const form = useFormContext<addressRegistrationValidationT>()
-  const fieldCityIsEmpty = cityValue.length !== 0
   const handleSelectCity = (city: string) => {
-    form.setValue('city', city)
+    console.log('ols')
     handleValueCity(city)
   }
+
+  const foundCity = data.filter(city =>
+    removeAccents(city.nome.toLowerCase()).includes(cityValue.toLowerCase()),
+  )
+
   if (isLoading)
     return (
       <div className=' mx-auto mt-5 h-5 w-5 animate-spin rounded-full  border-r-2 border-neutral-200' />
     )
   return (
-    <ul className='invisible-scrollbar absolute top-20 z-10 max-h-60 w-full overflow-y-auto rounded-md  border border-gray-200 bg-white'>
-      {data
-        .filter(city =>
-          removeAccents(city.nome.toLowerCase()).includes(
-            cityValue.toLowerCase(),
-          ),
-        )
-        .slice(0, 1)
-        .map(city => (
-          <ul
-            key={city.id}
-            className='relative flex min-h-[40px] w-full cursor-pointer items-center px-4 py-2 hover:bg-gray-200 '
-          >
-            <button
-              type='button'
-              className='w-full cursor-pointer px-4 py-2 text-left font-serif text-[0.625rem] font-medium  hover:bg-gray-200'
-              onClick={() => handleSelectCity(city.nome)}
-            >
-              <p className='text-base'>{fieldCityIsEmpty && city.nome}</p>
-            </button>
-          </ul>
-        ))}
+    <ul className='invisible-scrollbar relative   max-h-60 w-full max-w-sm overflow-y-auto  rounded-md border border-gray-200 bg-white'>
+      <li className='h-10 pb-1.5 pl-3 pt-2 '>
+        <button
+          type='button'
+          onClick={() => handleSelectCity(foundCity[0]?.nome)}
+          className='block w-full'
+        >
+          <p className='text-start font-notoSans text-sm text-neutral-500'>
+            {foundCity ? foundCity[0]?.nome : 'Cidade não encontrada'}
+          </p>
+          <Search
+            className={`absolute right-3.5 top-3 ${
+              disable ? 'fill-[#E5E7EB]' : 'fill-black'
+            } bg-slate-50`}
+          />
+        </button>
+      </li>
     </ul>
   )
 }
