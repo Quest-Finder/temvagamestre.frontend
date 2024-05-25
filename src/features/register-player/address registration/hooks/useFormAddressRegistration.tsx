02a4d5d32391fcf3ada4df06e2d04081e786c21b
Matheus <matheusmendesreis@outlook.com>
2024-05-25 14:09:25 -0300

    refactor

--- src/features/register-player/address registration/hooks/useFormAddressRegistration.tsx
+++ src/features/register-player/address registration/hooks/useFormAddressRegistration.tsx
@@ -4,21 +4,14 @@ import { useForm } from 'react-hook-form'
 import { addressRegistrationValidationT } from '../types/address-registration'
 import { addressRegistrationValidation } from '../utils/address-registration-validation'
 import { DEFAULT_VALEUS_ADDRESS_REGISTRATION } from '../utils/defaul-values'
-import ResetCityFieldWhenStateChanges from './resetCityFieldWhenStateChanges'
-import useLocalStorageGetItem from './useLocalStorageGetItem'
-import { useResetFieldsWhenLiveInBrazil } from './useResetFieldsWhenLiveInBrazil'
-import { useSetLiveInBrazilWhenStateOrCitySet } from './useSetLiveInBrazilWhenStateOrCitySet'
 
 export default function useFormAddressRegistration() {
-  const parsedData =
-    useLocalStorageGetItem<addressRegistrationValidationT>('form_data_adress')
+  // const parsedData =
+  //   useLocalStorageGetItem<addressRegistrationValidationT>('form_data_adress')
   const form = useForm<addressRegistrationValidationT>({
     resolver: zodResolver(addressRegistrationValidation),
-    defaultValues: parsedData || DEFAULT_VALEUS_ADDRESS_REGISTRATION,
+    defaultValues: DEFAULT_VALEUS_ADDRESS_REGISTRATION,
   })
-  useResetFieldsWhenLiveInBrazil(form)
-  useSetLiveInBrazilWhenStateOrCitySet(form)
-  ResetCityFieldWhenStateChanges(form)
 
   return { form }
 }
