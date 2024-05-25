443db0a935430d50486e8fd9a30bca35e2bdfbf9
Matheus <matheusmendesreis@outlook.com>
2024-05-25 13:55:55 -0300

    styled: adpter components for new styles

--- src/features/authentication/singIn.tsx
+++ src/features/authentication/singIn.tsx
@@ -1,19 +1,16 @@
-import { RegisterRoutes } from '@/services/routers'
 import { SignIn } from '@clerk/nextjs'
 
 export default function SingIn() {
   return (
     <SignIn
       path='/sign-in'
-      redirectUrl={RegisterRoutes.PlayerData}
       appearance={{
         elements: {
           socialButtons: 'flex flex-col',
           dividerText: 'hidden',
           header: 'flex items-center',
+          form: 'hidden',
           footer: 'hidden',
-          footerActionText: 'hidden',
-          footerActionLink: 'hidden',
         },
       }}
     />
