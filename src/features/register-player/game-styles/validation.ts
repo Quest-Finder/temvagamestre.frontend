28d323ea1b7d934716681e1639136ffa6fe4b45e
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-06-01 15:59:11 -0300

    fix: fixed checkbox array validation

--- src/features/register-player/game-styles/validation.ts
+++ src/features/register-player/game-styles/validation.ts
@@ -4,7 +4,7 @@ export const FormGameStylesSchema = z.object({
   rpgStyles: z.array(z.string()).refine((value) => value.some((item) => item), {
     message: "Você precisa escolher no mínimo 1 opção.",
   })
-    .refine((array) => array.length <=  3, {
+    .refine((array) => array.length >  3, {
     message: "No pode escolher máximo 3 opções"
   }),
 })
