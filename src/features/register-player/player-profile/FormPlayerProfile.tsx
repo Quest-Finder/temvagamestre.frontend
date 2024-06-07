fd91d6a9ab6a31544a211b5322bf5a556f782b8b
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-06-06 21:56:47 -0300

    feat: small change in card interaction. Refactor on form styles.

--- src/features/register-player/player-profile/FormPlayerProfile.tsx
+++ src/features/register-player/player-profile/FormPlayerProfile.tsx
@@ -17,8 +17,9 @@ import Image from 'next/image'
 import { mockFormPlayerProfile } from './mock'
 
 import { getRpgImage, getRpgStyling, getRpgImageColor } from './helpers/getRpgStyle'
-import { FormTitle } from "./components/FormTitle"
+import { FormTitle } from '../utils/title-form'
 import { FormAditionalText } from "./components/FormAditionalText"
+import { cn } from "@/lib/utils"
 
 export function FormPlayerProfile() {
   const form = useForm<FormPlayerProfileType>({
@@ -32,22 +33,22 @@ export function FormPlayerProfile() {
     console.log(data)
   }
 
-  console.log(form.getValues('playerProfileId'))
-
   return (
     <Form {...form}>
-      <FormAditionalText>
-        Olá! Para aprimorarmos sua experiência no TVM, por favor, responda algumas perguntas.
-      </FormAditionalText>
-      <FormTitle>
-        Escolha o perfil de jogador que mais se alinha com seus interesses.
-      </FormTitle>
+      <div>
+        <FormAditionalText>
+          Olá! Para aprimorarmos sua experiência no TVM, por favor, responda algumas perguntas.
+        </FormAditionalText>
+        <FormTitle>
+          Escolha o perfil de jogador que mais se alinha com seus interesses.
+        </FormTitle>
+      </div>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex flex-col items-center">
         <FormField
           control={form.control}
           name='playerProfileId'
           render={() => (
-            <FormItem className="flex gap-[1.625rem] space-y-0 max-w-[1125px]">
+            <FormItem className="flex gap-[1.625rem] space-y-0 max-w-[1125px] p-4">
               {mockFormPlayerProfile.map((item) => (
                 <FormField
                   key={item.id}
@@ -62,20 +63,25 @@ export function FormPlayerProfile() {
                         <FormControl>
                           <Card
                             aria-disabled={form.getValues("playerProfileId").length !== 0 && (form.getValues('playerProfileId') !== item.id)}
-                            className={`${getRpgStyling(item.title)} py-4 px-6 aria-disabled:opacity-50 cursor-pointer`}
+                            className={cn("py-4 px-6 space-y-6 h-full aria-disabled:opacity-50 cursor-pointer transition ease-in-out duration-300", getRpgStyling(item.title), field.value === item.id && 'shadow-[10px_10px_10px_0_rgb(0,0,0,0.25)]' )}
                             onClick={() => {
-                              console.log(form.getValues('playerProfileId'))
-                              return field.onChange(item.id)
+                              return field.value === item.id ? field.onChange('') : field.onChange(item.id)
                             }}
                           >
-                            <div className={`${getRpgImageColor(item.title)} rounded-full p-4 h-24 w-24 flex justify-center items-center`}>
+                            <div
+                              // className={`${getRpgImageColor(item.title)} rounded-full p-4 h-24 w-24 flex justify-center items-center`}
+                              className={cn("rounded-full p-4 h-24 w-24 flex justify-center items-center", getRpgImageColor(item.title))}
+                            >
                               <Image
                                 src={getRpgImage(item.title)}
+                                className={`${field.value === item.id && 'drop-shadow-lg'}`}
                                 alt={item.title}
                               />
                             </div>
-                            <CardTitle>{item.title}</CardTitle>
-                            <CardDescription>{item.description}</CardDescription>
+                            <div>
+                              <CardTitle className="text-base mb-1">{item.title}</CardTitle>
+                              <CardDescription className="text-base leading-6 text-neutral-700 break-">{item.description}</CardDescription>
+                            </div>
                           </Card>
                         </FormControl>
                       </FormItem>
@@ -90,7 +96,7 @@ export function FormPlayerProfile() {
         <div className='text-center'>
           <Button
             disabled={!form.formState.isValid}
-            className='mt-4 h-full max-h-14 w-full max-w-[214px] text-base disabled:opacity-50'
+            className='h-full max-h-14 w-full max-w-[214px] text-base disabled:opacity-50 transition ease-in-out duration-300'
             variant='default'
             type='submit'
           >
