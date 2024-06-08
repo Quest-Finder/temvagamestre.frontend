7804e035426e39016a959a63a10836c282a69720
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-06-07 21:34:59 -0300

    refactor: removed uneeded home button and removed button from game styles page

--- src/app/cadastro/estilos-de-jogo/page.tsx
+++ src/app/cadastro/estilos-de-jogo/page.tsx
@@ -1,19 +1,7 @@
 'use client'
 
 import { FormGameStyles } from '@/features/register-player/game-styles/FormGameStyles'
-import useSubmitFormRegister from '@/features/register-player/utils/submitFormRegister'
-import ButtonNavigation from '@/hooks/ButtonNavigation'
-import { RegisterRoutes } from '@/services/routers'
 
 export default function PagesStylesPlay() {
-  return (
-    <>
-      <FormGameStyles />
-      <ButtonNavigation
-        href={RegisterRoutes.Sucess}
-        content='sucesso'
-        onClick={useSubmitFormRegister}
-      />
-    </>
-  )
+  return <FormGameStyles />
 }
