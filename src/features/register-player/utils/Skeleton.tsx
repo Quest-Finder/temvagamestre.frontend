4fc67f873d899f3b59fae2bfb9480d13491129f3
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-06-09 17:28:55 -0300

    feat: created generic skeleton component

---
+++ src/features/register-player/utils/Skeleton.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils'
+
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  quantity: number
+  height: number | string
+  width: number | string
+}
+
+export function Skeleton({ className, quantity, height, width, ...props }: SkeletonProps) {
+  
+  const skeletonNumber = Array.from({ length: quantity }, (_, index) => index + 1)
+
+  return (
+    <>
+      {skeletonNumber.map((index) => (
+          <div
+            key={index}
+            className={cn("animate-pulse bg-neutral-200",`h-${height}`, `w-${width}`, className)}
+            {...props}
+          />
+        ))
+      }
+    </>
+  )
+}
