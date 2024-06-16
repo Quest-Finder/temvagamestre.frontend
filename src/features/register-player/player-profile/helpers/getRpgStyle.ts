9824aed504ff780cc6fe823612c6448bd8d7517d
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-06-16 19:42:43 -0300

    refactor: updated a better version of the conditional styling on playerProfile cards.

--- src/features/register-player/player-profile/helpers/getRpgStyle.ts
+++ src/features/register-player/player-profile/helpers/getRpgStyle.ts
@@ -2,35 +2,26 @@ import Exploracao from '../assets/Exploracao.svg'
 import MatarEPilhar from '../assets/MatarEPilhar.svg'
 import Interpretacao from '../assets/Interpretacao.svg'
 
-export function getRpgImage(rpgStyle: string) {
-  switch (rpgStyle) {
-    case 'Exploração':
-      return Exploracao
-    case 'Matar e pilhar': 
-      return MatarEPilhar
-    case 'Interpretação':
-      return Interpretacao
+import { RpgStyles, RpgStyleData } from '../types/palyerProfileTypes'
+
+const rpgStyles: { [key in RpgStyles]: RpgStyleData} = {
+  'Exploração': {
+    image: Exploracao,
+    colorClass: 'bg-primary-100',
+    stylingClass: 'border-primary-500 bg-primary-50',
+  },
+  'Matar e pilhar': {
+    image: MatarEPilhar,
+    colorClass: 'bg-secondary-100',
+    stylingClass: 'border-secondary-500 bg-secondary-50',
+  },
+  'Interpretação': {
+    image: Interpretacao,
+    colorClass: 'bg-accent-100',
+    stylingClass: 'border-accent-500 bg-accent-50',
   }
 }
 
-export function getRpgImageColor(rpgStyle: string) {
-  switch (rpgStyle) {
-    case 'Exploração':
-      return 'bg-primary-100'
-    case 'Matar e pilhar': 
-      return 'bg-secondary-100'
-    case 'Interpretação':
-      return 'bg-accent-100'
-  }
-}
-
-export function getRpgStyling(rpgStyle: string) {
-  switch (rpgStyle) {
-    case 'Exploração':
-      return 'border-primary-500 bg-primary-50'
-    case 'Matar e pilhar': 
-      return 'border-secondary-500 bg-secondary-50'
-    case 'Interpretação':
-      return 'border-accent-500 bg-accent-50'
-  }
+export function getRpgStyling(rpgStyle: RpgStyles) {
+  return rpgStyles[rpgStyle]
 }
--- src/features/register-player/player-profile/types/palyerProfileTypes.ts
+++ src/features/register-player/player-profile/types/palyerProfileTypes.ts
@@ -1,11 +1,19 @@
 import { z } from "zod"
 import { FormPlayerProfileSchema } from "../validation"
-
+import { StaticImport } from "next/dist/shared/lib/get-img-props"
 
 export interface PlayerProfileType {
   id: string,
-  title: string,
+  title: RpgStyles,
   description: string
 }
+
+export interface RpgStyleData {
+  image: StaticImport | string,
+  colorClass: string
+  stylingClass: string
+}
+
+export type RpgStyles = 'Exploração' | 'Matar e pilhar' | 'Interpretação';
 
 export type FormPlayerProfileType = z.infer<typeof FormPlayerProfileSchema>
