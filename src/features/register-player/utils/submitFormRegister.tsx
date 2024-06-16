d731ccfde8e9ec17c9d620b67b722b48cd3e3bd0
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-06-16 19:43:36 -0300

    feat: updated submit form to to recieve aditional data.

--- src/features/register-player/utils/submitFormRegister.tsx
+++ src/features/register-player/utils/submitFormRegister.tsx
@@ -3,6 +3,9 @@ import { TValidation } from '../about-you/types/validation'
 import { validateAboutYou } from '../about-you/utils/validation'
 import useLocalStorageGetItem from '../address registration/hooks/useLocalStorageGetItem'
 import { addressRegistrationValidationT } from '../address registration/types/address-registration'
+import { addressRegistrationValidation } from '../address registration/utils/address-registration-validation'
+import { getPlayerGameStylesData } from '../player-profile/helpers/getPlayerGameStylesData'
+import { FormPlayerProfileSchema } from '../player-profile/validation'
 import {
   FormStepOneSchema,
   FormStepOneType,
@@ -13,17 +16,23 @@ import { socialNetworkValidation } from '../social network registration/utils/so
 function getFormsInLocalStrage() {
   const dateFormOne = useLocalStorageGetItem<FormStepOneType>('form_step_one')
   const dateFormTwo =
-    useLocalStorageGetItem<TsocialNetworkValidation>('form_data_adress')
+    useLocalStorageGetItem<TsocialNetworkValidation>('form_data_social_network')
   const dateFormTree = useLocalStorageGetItem<TValidation>(
     'form_data_about_you',
   )
   const dateFormFour =
     useLocalStorageGetItem<addressRegistrationValidationT>('form_data_adress')
+  // const dateFormFive = 
+  //   useLocalStorageGetItem('game_styles')
+  const dateFormSix = 
+    getPlayerGameStylesData()
   const mergedData = {
     ...dateFormOne,
     ...dateFormTwo,
     ...dateFormTree,
     ...dateFormFour,
+    // ...dateFormFive,
+    ...dateFormSix,
   }
   return { mergedData }
 }
@@ -33,6 +42,7 @@ function clearFormsInLocalStorage() {
   localStorage.removeItem('form_data_adress')
   localStorage.removeItem('form_data_social_network')
   localStorage.removeItem('form_data_about_you')
+  localStorage.removeItem('game_styles')
 }
 
 export default async function useSubmitFormRegister() {
@@ -40,7 +50,10 @@ export default async function useSubmitFormRegister() {
   const resultOne = FormStepOneSchema.safeParse(mergedData)
   const resultTwo = socialNetworkValidation.safeParse(mergedData)
   const resultThree = validateAboutYou.safeParse(mergedData)
-  if (!resultOne.success && !resultTwo.success && !resultThree.success) {
+  const resultFour = addressRegistrationValidation.safeParse(mergedData)
+  // const resultFive = FormGameStylesSchema.safeParse(mergedData)
+  const resultSix = FormPlayerProfileSchema.safeParse(mergedData)
+  if (!resultOne.success && !resultTwo.success && !resultThree.success && !resultFour && !resultSix) {
     return null
   }
   try {
