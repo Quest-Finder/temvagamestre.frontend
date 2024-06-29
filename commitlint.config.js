a3020f9f9bc6f1d680704f15befe748aaf374b5d
matheus <matheusmendesreis@outlook.com>
2024-06-29 14:53:19 -0300

    feat: commits lower case

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', 'lower-case'], // Configura para sempre exigir letras minúsculas no subject
+  },
 }
