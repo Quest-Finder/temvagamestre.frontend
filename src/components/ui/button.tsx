44fc1a77850444a55b235065e56dad6d64fc83c3
Matheus Mendes <matheusmendesreis@outlook.com>
2024-07-04 20:38:18 -0300

    feat: update ghost styled

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react'
+import { cn } from '@/lib/utils'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
-
-import { cn } from '@/lib/utils'
+import * as React from 'react'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 leading-5',
@@ -20,9 +19,10 @@ const buttonVariants = cva(
         outline:
           'border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50',
         ghost:
-          'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+          'font-mono min-h-[52px] w-full max-w-[281px] rounded-md p-4 transition-all hover:bg-[#CCFBF1] hover:text-[#7C7C7C] font-notoSans font-normal font-normal text-sm leading-5',
         link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
       },
+
       size: {
         default: 'h-10 px-8 py-4',
         sm: 'h-9 rounded-md px-3',
