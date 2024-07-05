3b06ffdbef788322cd43f95a894a73641db57119
Matheus Mendes <matheusmendesreis@outlook.com>
2024-07-05 16:26:13 -0300

    feat: function that selects social media icons

--- src/features/register-player/social network registration/utils/get-icon-by-name.ts
+++ src/features/register-player/social network registration/utils/get-icon-by-name.ts
@@ -4,17 +4,21 @@ import { InstagramIcon } from '@/components/icons/instagram'
 import ReeditIcon from '@/components/icons/reeditIcon'
 import XIcon from '@/components/icons/xIcon'
 
-export function getIconByName(socialMedia: string) {
+type SocialMediaName =
+  | 'facebook'
+  | 'instagram'
+  | 'twitter'
+  | 'reddit'
+  | 'discord'
+export function getIconByName(socialMedia: SocialMediaName) {
   switch (socialMedia) {
     case 'facebook':
       return FacebookIcon
     case 'instagram':
       return InstagramIcon
     case 'twitter':
-      return FacebookIcon
-    case 'reddit':
       return XIcon
-    case 'ReeditIcon':
+    case 'reddit':
       return ReeditIcon
     case 'discord':
       return Discord
