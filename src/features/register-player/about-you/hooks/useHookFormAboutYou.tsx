0266f8350b4c1be4cdbeff3017c2684b96362ade
Matheus Mendes <matheusmendesreis@outlook.com>
2024-07-05 19:45:11 -0300

    feat: add key in variable

--- src/features/register-player/about-you/hooks/useHookFormAboutYou.tsx
+++ src/features/register-player/about-you/hooks/useHookFormAboutYou.tsx
@@ -1,17 +1,28 @@
+import { RegisterRoutes } from '@/services/routers'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import useLocalStorageGetItem from '../../address registration/hooks/useLocalStorageGetItem'
+import uselocalStorageSetItem from '../../address registration/hooks/uselocalStorageSetItem'
 import { TValidation } from '../types/validation'
 import { defaultValuesAboutYou } from '../utils/default-values'
 import { validateAboutYou } from '../utils/validation'
 
 export default function useHookFormAboutYou() {
-  const parsedData = useLocalStorageGetItem('form_data_about_you')
+  const router = useRouter()
+  const keyLocalStorage = 'form_step_tree'
+
+  const parsedData = useLocalStorageGetItem(keyLocalStorage)
 
   const form = useForm<TValidation>({
     resolver: zodResolver(validateAboutYou),
     defaultValues: parsedData || defaultValuesAboutYou,
     mode: 'onChange',
   })
-  return form
+
+  function onSubmit(values: TValidation) {
+    uselocalStorageSetItem(keyLocalStorage, values)
+    router.push(RegisterRoutes.Adress)
+  }
+  return { form, onSubmit }
 }
