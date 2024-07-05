4202f1458239c36d4098dd4ad5be72ec69e7ddf6
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-07-04 22:38:51 -0300

    fixed: duplicated component that was created on another branch but it leaked through

--- src/features/register-player/player-profile/validation.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-
-export const FormPlayerProfileSchema = z.object({
-  playerProfileId: z.string()
-    .min(1, { message: 'Necessário escolher pelo menos uma opção' })
-})
-
-export type FormPlayerProfileType = z.infer<typeof FormPlayerProfileSchema>
