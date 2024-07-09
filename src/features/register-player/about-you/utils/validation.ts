e323209d1f6a2b37fd09b2d4edc1cc27b57e777a
Matheus Mendes <matheusmendesreis@outlook.com>
2024-07-09 18:10:34 -0300

    feat: corrigido a quantidade maxima de caracteres do titulo e definido os campos como opcionais.

--- src/features/register-player/about-you/utils/validation.ts
+++ src/features/register-player/about-you/utils/validation.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
 export const validateAboutYou = z.object({
-  titulo: z.string().min(3, 'Nome é obrigatório').max(500),
-  bio: z.string().min(3, 'Bio é obrigatório').max(500),
+  titulo: z.string().min(3, 'Nome é obrigatório').max(40).optional(),
+  bio: z.string().min(3, 'Bio é obrigatório').max(500).optional(),
 })
