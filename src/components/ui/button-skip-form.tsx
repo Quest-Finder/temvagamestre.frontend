802be58ef7e16ae0a0dc62cd71ec08d6ff66d6dc
Matheus Mendes <matheusmendesreis@outlook.com>
2024-07-09 22:26:53 -0300

    refactor: aplicando correções sugeridas

--- src/components/ui/button-skip-form.tsx
+++ src/components/ui/button-skip-form.tsx
@@ -9,9 +9,9 @@ export function ButtonSkipForm({ content, href }: Props) {
   return (
     <Link
       href={href}
-      className=' rounded-md; order-none flex h-[3.25rem] w-[17.5625rem] flex-none grow-0 flex-row items-center justify-center gap-2.5 p-4 hover:bg-[#CCFBF1]'
+      className='rounded-md order-none flex h-[3.25rem] w-[17.5625rem] flex-none grow-0 flex-row items-center justify-center gap-2.5 p-4 hover:bg-[#CCFBF1]'
     >
-      <span className=' e h-5 text-sm font-normal not-italic leading-5 text-[#7C7C7C]'>
+      <span className='h-5 text-sm font-normal not-italic leading-5 text-[#7C7C7C]'>
         {content}
       </span>
     </Link>
