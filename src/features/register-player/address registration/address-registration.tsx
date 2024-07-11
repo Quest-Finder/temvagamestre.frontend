061ca05d75489ac782792156e947d301a9e1ab0c
Matheus Mendes <matheusmendesreis@outlook.com>
2024-07-11 15:23:32 -0300

    refactor: corrigindo nome do componente,  adicionado  btn desabilitado caso o form seja invalido,melhorias no css

--- src/features/register-player/address registration/address-registration.tsx
+++ src/features/register-player/address registration/address-registration.tsx
@@ -1,38 +1,38 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { RegisterRoutes } from '@/services/routers'
 import { FormProvider } from 'react-hook-form'
+import * as FormText from '../FormText.json'
 import { FormTitle } from '../utils/title-form'
-import InputLiveInBrazil from './components/input-checkbox-live-in-brazil'
+import CheckboxLiveAbroad from './components/ checkbox-live-abroad'
 import InputCity from './components/input-city'
 import InputState from './components/input-state'
 import useFormAddressRegistration from './hooks/useFormAddressRegistration'
-import useSubmitFormAddressRegistration from './service/submitFormAddressRegistration'
-import * as FormText from '../FormText.json'
 
 export default function FormAddressRegistration() {
-  const { form } = useFormAddressRegistration()
+  const { form, onSubmit } = useFormAddressRegistration()
+  const isDisable = !form.formState.isValid
   return (
     <FormProvider {...form}>
       <FormTitle>{FormText.addressRegistration.title}</FormTitle>
       <form
-        onSubmit={form.handleSubmit(
-          useSubmitFormAddressRegistration(form, RegisterRoutes.StylesPlay),
-        )}
+        onSubmit={form.handleSubmit(onSubmit)}
         className='mx-auto flex  min-h-[31.25rem] w-full max-w-[23.1875rem] flex-col items-center justify-between'
       >
-        <div className='flex w-full flex-col gap-2'>
+        <div className='flex w-full flex-col items-center justify-center gap-2'>
           <InputState />
-          <InputCity />
+          <div className='flex w-full max-w-[340px] flex-col items-center justify-center gap-2'>
+            <InputCity />
+          </div>
         </div>
 
         <div className=' flex w-full flex-col gap-8'>
-          <InputLiveInBrazil />
+          <CheckboxLiveAbroad />
           <div className='flex flex-col items-center gap-8'>
             <Button
               type='submit'
               className=' min-h-[3.5rem] w-full min-w-[6.25rem] max-w-[13.375rem] flex-wrap px-8 py-4'
+              disabled={isDisable}
             >
               Salvar e Continuar
               {/* <ArrowLeft /> */}
