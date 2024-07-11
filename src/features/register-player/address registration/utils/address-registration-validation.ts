f6f30cf98f71ac13140eb959663416c09d8d2054
Matheus Mendes <matheusmendesreis@outlook.com>
2024-07-11 15:14:48 -0300

    refactor:mudanca no nome do atributo 'liveInBrasil' para 'liveAbroad' e adiconei o refine para controlar a validação do form confomer o valor do liveAbroad

--- src/features/register-player/address registration/utils/address-registration-validation.ts
+++ src/features/register-player/address registration/utils/address-registration-validation.ts
@@ -1,7 +1,21 @@
 import { z } from 'zod'
 
-export const addressRegistrationValidation = z.object({
-  state: z.string().max(2).optional(),
-  city: z.string().max(255).optional(),
-  liveInBrazil: z.boolean().optional(),
-})
+export const addressRegistrationValidation = z
+  .object({
+    state: z.string().max(2).optional(),
+    city: z.string().max(255).optional(),
+    liveAbroad: z.boolean().default(false),
+  })
+  .refine(
+    data =>
+      // caso more fora do brasil
+      (data.liveAbroad === true && (!data.state || !data.city)) ||
+      // caso more no brasil
+      (data.liveAbroad === false && data.state && data.city),
+
+    {
+      message:
+        "Se 'liveAbroad' for verdadeiro, 'state' e 'city' devem estar vazios. Se 'liveAbroad' for falso, 'state' e 'city' devem estar preenchidos.",
+      path: ['liveAbroad', 'state', 'city'],
+    },
+  )
