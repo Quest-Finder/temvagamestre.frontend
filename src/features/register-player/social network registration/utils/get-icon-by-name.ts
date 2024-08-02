0320473cf5001dc2890560d39eb732bbefef364b
Matheus Mendes <matheusmendesreis@outlook.com>
2024-08-01 22:39:30 -0300

    fix: remove unused dependency "RedditIcon"

--- src/features/register-player/social network registration/utils/get-icon-by-name.ts
+++ src/features/register-player/social network registration/utils/get-icon-by-name.ts
@@ -1,7 +1,6 @@
 import Discord from '@/components/icons/discord'
 import FacebookIcon from '@/components/icons/facebookIcon'
 import { InstagramIcon } from '@/components/icons/instagram'
-import RedditIcon from '@/components/icons/reeditIcon'
 import XIcon from '@/components/icons/xIcon'
 
 export function getIconByName(socialMedia: string) {
