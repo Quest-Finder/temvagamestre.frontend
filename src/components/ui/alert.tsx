032582f3908c4fb4965d416f8e77e256798b2c9d
Juliano Sill <sill.juliano@gmail.com>
2024-08-09 23:53:11 -0300

    feat(components): create alert

---
+++ src/components/ui/alert.tsx
@@ -0,0 +1,73 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+
+const alertVariants = cva('w-full rounded-lg border border-border p-4', {
+  variants: {
+    variant: {
+      default: 'text-foreground',
+      error: 'border-error bg-error-soft/20 text-error dark:text-error-hard',
+    },
+  },
+  defaultVariants: {
+    variant: 'error',
+  },
+})
+
+const AlertRoot = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
+  <div
+    ref={ref}
+    role='alert'
+    className={cn(alertVariants({ variant }), className)}
+    {...props}
+  />
+))
+AlertRoot.displayName = 'Alert'
+
+export interface AlertTitleProps
+  extends React.HTMLAttributes<HTMLHeadingElement> {}
+const AlertTitle = React.forwardRef<HTMLParagraphElement, AlertTitleProps>(
+  ({ className, children, ...props }, ref) => (
+    <h5
+      ref={ref}
+      className={cn('mb-1 font-semibold leading-tight', className)}
+      {...props}
+    >
+      {children}
+    </h5>
+  ),
+)
+AlertTitle.displayName = 'AlertTitle'
+
+export interface AlertDescriptionProps
+  extends React.HTMLAttributes<HTMLParagraphElement> {}
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  AlertDescriptionProps
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn('text-sm', className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = 'AlertDescription'
+
+export const Alert = {
+  Root: AlertRoot,
+  Title: AlertTitle,
+  Description: AlertDescription,
+}
+
+/* USAGE
+
+  <Alert.Root>
+    <Alert.Title/>
+    <Alert.Description/>
+  </Alert.Root>
+
+*/
