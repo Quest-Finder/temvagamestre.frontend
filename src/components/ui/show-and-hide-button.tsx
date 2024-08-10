eddde3e42ae794a6b58178eb45d276dad847d252
Juliano Sill <sill.juliano@gmail.com>
2024-08-09 23:52:22 -0300

    feat(components): create show/hide button

---
+++ src/components/ui/show-and-hide-button.tsx
@@ -0,0 +1,34 @@
+import { Eye, EyeOff } from 'lucide-react'
+import type { ComponentProps } from 'react'
+
+import { cn } from '@/lib/utils'
+
+interface ShowAndHideButtonProps extends ComponentProps<'button'> {
+  title: string
+  show: boolean
+}
+
+export function ShowAndHideButton({
+  title,
+  show,
+  className,
+  ...props
+}: ShowAndHideButtonProps) {
+  return (
+    <button
+      type='button'
+      className={cn(
+        'absolute right-1 top-1 flex size-8 items-center justify-center rounded-md text-foreground-soft ring-offset-background transition-colors hover:text-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+        className,
+      )}
+      {...props}
+    >
+      {show ? (
+        <EyeOff className='size-5 shrink-0' />
+      ) : (
+        <Eye className='size-5 shrink-0' />
+      )}
+      <span className='sr-only'>{title}</span>
+    </button>
+  )
+}
