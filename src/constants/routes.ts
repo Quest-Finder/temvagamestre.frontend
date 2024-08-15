7ca530533e4ab8df4e0b6d0d9b7593d5e9b75da0
Juliano Sill <sill.juliano@gmail.com>
2024-08-15 09:51:59 -0300

    feat(constants): add register player routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,3 +2,8 @@ export const AUTH_ROUTES = {
   SIGN_IN: '/auth/sign-in',
   SIGN_UP: '/auth/sign-up',
 } as const
+
+export const REGISTER_PLAYER_ROUTES = {
+  ONBOARDING: '/register/onboarding',
+  PLAYER: '/register/player',
+} as const
