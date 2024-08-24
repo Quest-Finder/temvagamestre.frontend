9f05d6ed4c66c45cf28a6b57573864add237c711
Juliano Sill <sill.juliano@gmail.com>
2024-08-23 22:16:08 -0300

    chore(middleware): change logic to declare public routes instead of private

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher([])
-// const isProtectedRoute = createRouteMatcher(['/cadastro(.*)'])
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-})
+import { AUTH_PREFIX } from './constants/routes'
+
+const isPublicRoute = createRouteMatcher(['/', `${AUTH_PREFIX}(.*)`])
+
+export default clerkMiddleware(
+  (auth, request) => {
+    const isPrivateRoute = !isPublicRoute(request)
+    if (!auth().userId && isPrivateRoute) auth().redirectToSignIn()
+  },
+  // Enable terminal logs when in development environment
+  { debug: process.env.NODE_ENV === 'development' },
+)
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
